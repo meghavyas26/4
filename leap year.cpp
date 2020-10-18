b7de420d04eacc884a2ea296987601608c7c4f4d
varunn10 <39978783+varunn10@users.noreply.github.com>
2020-10-18 18:42:10 +0530

    Add files via upload

---
+++ leap year.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int y,l;
+	printf("ENTER A YEAR ;");
+	scanf("%d",&y);
+	if(y%100==0)
+	{
+		if(y%400==0)
+		{
+			l=1;
+		}
+	}
+	else if(y%4==0)
+	{
+		l=1;
+	}
+	else
+	{
+		l=0;
+	}
+	if(l==1)
+	{
+		printf("leap");
+	}
+	else
+	{
+		printf("not leap");
+	}
+}
